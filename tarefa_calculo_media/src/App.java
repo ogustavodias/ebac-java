d326776e9d7fc4c6f0430a8b65b49691482d0486
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-08-13 14:30:56 -0300

    método alterado para uso da classe Scanner.

--- tarefa_calculo_media/src/App.java
+++ tarefa_calculo_media/src/App.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(getAverage(10, 8, 5, 4));
+        printAverage();
     }
 
-    public static float getAverage(float n1, float n2, float n3, float n4) {
-        return (n1 + n2 + n3 + n4) / 4;
+    public static void printAverage() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        float n1 = in.nextFloat();
+        System.out.println("Digite o segundo número: ");
+        float n2 = in.nextFloat();
+        System.out.println("Digite o terceiro número: ");
+        float n3 = in.nextFloat();
+        System.out.println("Digite o quarto número: ");
+        float n4 = in.nextFloat();
+
+        float average = (n1 + n2 + n3 + n4) / 4;
+        System.out.println("A média dos números digitados é " + average);
+
+        in.close();
     }
 }
