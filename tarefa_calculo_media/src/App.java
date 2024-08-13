d218d59f2ba57cad04857fcb14565551eb3225f0
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-08-13 14:31:39 -0300

    reset

--- tarefa_calculo_media/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println(getAverage(10, 8, 5, 4));
-    }
-
-    public static float getAverage(float n1, float n2, float n3, float n4) {
-        return (n1 + n2 + n3 + n4) / 4;
-    }
-}
