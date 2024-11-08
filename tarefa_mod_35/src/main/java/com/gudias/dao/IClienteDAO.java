df7e009d27bec3db22dd4031ff454af3874401ef
ogustavodias <gustavoalvesdiaz08@gmail.com>
2024-11-07 21:12:29 -0300

    tarefa concluída

--- tarefa_mod_35/src/main/java/com/gudias/dao/IClienteDAO.java
+++
@@ -1,9 +0,0 @@
-package com.gudias.dao;
-
-import com.gudias.dao.generic.IGenericDAO;
-import com.gudias.domain.Cliente;
-
-public interface IClienteDAO extends IGenericDAO<Cliente, Long> {
-
-
-}
--- tarefa_mod_35/src/main/java/com/gudias/dao/IProdutoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.gudias.dao;
-
-import com.gudias.dao.generic.IGenericDAO;
-import com.gudias.domain.Produto;
-
-
-public interface IProdutoDAO extends IGenericDAO<Produto, String>{
-
-}
